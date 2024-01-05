5f40b3ce3286d4afa31e634445be71705adcc0cb
fkaaziebu <frederickaziebu1998@gmail.com>
2024-01-05 19:40:09 +0000

    fix: minor fixes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const analyzeRoutes = require("./routes/analyze");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 // API ENDPOINTS
